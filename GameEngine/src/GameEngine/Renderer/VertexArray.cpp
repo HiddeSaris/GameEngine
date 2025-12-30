486d1df85e3c795303e62e9665a73d5ce0f4395b
HiddeSaris <115933@leerling.stedelijkgymnijmegen.nl>
2025-12-30 20:13:52 +0100

    Draw Call Abstraction

--- GameEngine/src/GameEngine/Renderer/VertexArray.cpp
+++ GameEngine/src/GameEngine/Renderer/VertexArray.cpp
@@ -2,6 +2,7 @@
 #include "VertexArray.h"
 
 #include "Renderer.h"
+#include "RendererAPI.h"
 #include "Platform/OpenGL/OpenGLVertexArray.h"
 
 namespace GameEngine {
@@ -10,8 +11,8 @@ namespace GameEngine {
 	{
 		switch (Renderer::GetAPI())
 		{
-		case RendererAPI::None: GE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::OpenGL: return new OpenGLVertexArray();
+		case RendererAPI::API::None: GE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL: return new OpenGLVertexArray();
 		}
 
 		GE_CORE_ASSERT(false, "Unknown RendererAPI!");
