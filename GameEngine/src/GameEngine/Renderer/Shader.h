1a101be9263afc26279af287546f3357c65c4526
HiddeSaris <115933@leerling.stedelijkgymnijmegen.nl>
2026-01-01 19:01:12 +0100

    Shader files

--- GameEngine/src/GameEngine/Renderer/Shader.h
+++ GameEngine/src/GameEngine/Renderer/Shader.h
@@ -12,7 +12,9 @@ namespace GameEngine {
 		virtual void Bind() const = 0;
 		virtual void UnBind() const = 0;
 
+		static Shader* Create(const std::string& filepath);
 		static Shader* Create(const std::string& vertexSrc, const std::string& fragmentSrc);
+
 	};
 
 }
