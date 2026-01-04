36030507fdf905b2f8b4d4661ce8b8ed287c4860
HiddeSaris <115933@leerling.stedelijkgymnijmegen.nl>
2026-01-04 10:44:25 +0100

    Profiling

--- GameEngine/src/GameEngine/ImGui/ImGuiLayer.cpp
+++ GameEngine/src/GameEngine/ImGui/ImGuiLayer.cpp
@@ -83,9 +83,4 @@ namespace GameEngine {
 		}
 	}
 
-	void ImGuiLayer::OnImGuiRender()
-	{
-		static bool show = true;
-		//ImGui::ShowDemoWindow(&show);
-	}
 }
--- SandBox/src/Sandbox2D.cpp
+++ SandBox/src/Sandbox2D.cpp
@@ -4,6 +4,46 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <chrono>
+
+template<typename Fn>
+class Timer
+{
+public:
+	Timer(const char* name, Fn&& func)
+		: m_Name(name), m_Func(func), m_Stopped(false)
+	{
+		m_StartTimepoint = std::chrono::high_resolution_clock::now();
+	}
+
+	~Timer()
+	{
+		if (!m_Stopped)
+			Stop();
+	}
+
+	void Stop()
+	{
+		auto endTimepoint = std::chrono::high_resolution_clock::now();
+
+		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+
+		m_Stopped = true;
+
+		float duration = (end - start) * 0.001f;
+		m_Func({ m_Name, duration });
+	}
+
+private:
+	const char* m_Name;
+	Fn m_Func;
+	std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
+	bool m_Stopped;
+};
+
+#define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult) { m_ProfileResults.push_back(profileResult); })
+
 Sandbox2D::Sandbox2D()
 	: Layer("Sandbox2D"), m_CameraController(16.0f / 9.0f)
 {
@@ -21,20 +61,29 @@ void Sandbox2D::OnDetach()
 
 void Sandbox2D::OnUpdate(GameEngine::Timestep dt)
 {
-	m_CameraController.OnUpdate(dt);
+	PROFILE_SCOPE("Sandbox2D::OnUpdate");
 
-	GameEngine::RenderCommand::SetClearColor({ 0.0f, 0.1f, 0.1f, 1.0f });
-	GameEngine::RenderCommand::Clear();
+	{
+		PROFILE_SCOPE("m_CameraController::OnUpdate");
+		m_CameraController.OnUpdate(dt);
+	}
 
-	GameEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
+	{
+		PROFILE_SCOPE("Renderer Prep");
+		GameEngine::RenderCommand::SetClearColor({ 0.0f, 0.1f, 0.1f, 1.0f });
+		GameEngine::RenderCommand::Clear();
+	}
 
-	GameEngine::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f }, m_Rotation);
-	GameEngine::Renderer2D::DrawQuad({ 0.55f, -0.55f }, { 0.5f, 0.75f }, m_SquareColor, m_Rotation * -3);
-	GameEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_Texture, {0.4f, 0.8f, 0.9f, 1.0f});
+	{
+		PROFILE_SCOPE("Renderer Draw");
+		GameEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-	GameEngine::Renderer2D::EndScene();
+		GameEngine::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f }, m_Rotation);
+		GameEngine::Renderer2D::DrawQuad({ 0.55f, -0.55f }, { 0.5f, 0.75f }, m_SquareColor, m_Rotation * -3);
+		GameEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_Texture, { 0.4f, 0.8f, 0.9f, 1.0f });
 
-	GE_TRACE(m_Rotation);
+		GameEngine::Renderer2D::EndScene();
+	}
 
 	m_Rotation += 0.1f;
 	
@@ -44,6 +93,16 @@ void Sandbox2D::OnImGuiRender()
 {
 	ImGui::Begin("Settings");
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
+
+	for (auto& result : m_ProfileResults)
+	{
+		char label[50];
+		strcpy(label, "%.3fms ");
+		strcat(label, result.Name);
+		ImGui::Text(label, result.Time);
+	}
+	m_ProfileResults.clear();
+
 	ImGui::End();
 }
 
